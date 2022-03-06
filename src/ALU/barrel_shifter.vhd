4a4ab9483ad7de81dd964f2f15ac0fa2373db343
jamesminardi <jamesminardip@gmail.com>
2022-03-05 20:56:04 -0600

    Fix syntax errors

--- src/ALU/barrel_shifter.vhd
+++ src/ALU/barrel_shifter.vhd
@@ -7,7 +7,7 @@
 -- TODO: Add left/right functionality and
 -- arithmatic vs logical functionality
 --
--- REQUIRES: MIPS_types.vhd
+-- REQUIRES: MIPS_types.vhd mux2t1_N.vhd mux2t1.vhd
 -------------------------------------------------------------------------
 
 -- library declaration
@@ -19,6 +19,7 @@ library work;
 use work.MIPS_types.all;
 
 entity barrel_shifter is
+generic(N : integer := 32);
     port(
         iA : in std_logic_vector(DATA_WIDTH - 1 downto 0);
         iLeft : in std_logic;
@@ -28,7 +29,6 @@ entity barrel_shifter is
 end barrel_shifter;
 
 architecture mixed of barrel_shifter is
-    generic(N : integer := 32);
     component mux2t1_N is
         port(
         i_S    : in std_logic;
@@ -59,7 +59,10 @@ begin
 
     with iLeft select
         s_iA <=
-            iA(0 to DATA_WIDTH - 1) when '1',
+             iA(0) &iA(1) &iA(2) &iA(3) &iA(4) &iA(5) &iA(6) &iA(7)
+            &iA(8) &iA(9) &iA(10)&iA(11)&iA(12)&iA(13)&iA(14)&iA(15)
+            &iA(16)&iA(17)&iA(18)&iA(19)&iA(20)&iA(21)&iA(22)&iA(23)
+            &iA(24)&iA(25)&iA(26)&iA(27)&iA(28)&iA(29)&iA(30)&iA(31) when '1',
             iA when others;
 
     s_shifted_16b(31 downto 16) <= (others => s_ext);
@@ -74,12 +77,11 @@ begin
     s_shifted_2b(31 downto 30)  <= (others => s_ext);
     s_shifted_2b(29 downto 0)   <= s_result_4b(31 downto 2);
 
-    s_shifted_1b(31)            <= (others => s_ext);
+    s_shifted_1b(31)            <= s_ext;
     s_shifted_1b(30 downto 0)   <= s_result_2b(31 downto 1);
 
 
     MUX2t1_N_16b: mux2t1_N
-        generic map(N => DATA_WIDTH);
         port map(
             i_S  => iShamt(4),
             i_D0 => iA,
@@ -87,43 +89,39 @@ begin
             o_O  => s_result_16b);
 
     MUX2t1_N_8b: mux2t1_N
-        generic map(N => DATA_WIDTH);
         port map(
             i_S  => iShamt(3),
             i_D0 => s_result_16b,
-            i_D1 => s_shifted_8b
+            i_D1 => s_shifted_8b,
             o_O  => s_result_8b);
 
     MUX2t1_N_4b: mux2t1_N
-        generic map(N => DATA_WIDTH);
         port map(
             i_S  => iShamt(2),
             i_D0 => s_result_8b,
-            i_D1 => s_shifted_4b
+            i_D1 => s_shifted_4b,
             o_O  => s_result_4b);
 
     MUX2t1_N_2b: mux2t1_N
-        generic map(N => DATA_WIDTH);
         port map(
             i_S  => iShamt(1),
             i_D0 => s_result_4b,
-            i_D1 => s_shifted_2b
+            i_D1 => s_shifted_2b,
             o_O  => s_result_2b);
 
     MUX2t1_N_1b: mux2t1_N
-        generic map(N => DATA_WIDTH);
         port map(
             i_S  => iShamt(0),
             i_D0 => s_result_2b,
-            i_D1 => s_shifted_1b
+            i_D1 => s_shifted_1b,
             o_O  => s_result_1b);
-
 
     with iLeft select
         oResult <=
-            s_result_1b(0 to DATA_WIDTH - 1) when '1',
+             s_result_1b(0) &s_result_1b(1) &s_result_1b(2) &s_result_1b(3) &s_result_1b(4) &s_result_1b(5) &s_result_1b(6) &s_result_1b(7)
+            &s_result_1b(8) &s_result_1b(9) &s_result_1b(10)&s_result_1b(11)&s_result_1b(12)&s_result_1b(13)&s_result_1b(14)&s_result_1b(15)
+            &s_result_1b(16)&s_result_1b(17)&s_result_1b(18)&s_result_1b(19)&s_result_1b(20)&s_result_1b(21)&s_result_1b(22)&s_result_1b(23)
+            &s_result_1b(24)&s_result_1b(25)&s_result_1b(26)&s_result_1b(27)&s_result_1b(28)&s_result_1b(29)&s_result_1b(30)&s_result_1b(31) when '1',
             s_result_1b when others;
-
-    oResult <= s_result_1b;
 
 end mixed;
