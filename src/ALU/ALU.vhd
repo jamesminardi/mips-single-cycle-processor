9ae646b3fd3dd636b25674054e40f16b86d96aae
jamesminardi <jamesminardip@gmail.com>
2022-03-10 23:29:22 -0600

    Compiles

--- src/ALU/ALU.vhd
+++ src/ALU/ALU.vhd
@@ -17,8 +17,8 @@ use work.MIPS_types.all;
 
 entity ALU is
     port (
-        iX1 : in std_logic_vector(DATA_WIDTH - 1 downto 0);
-        iX2 : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+        iA : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+        iB : in std_logic_vector(DATA_WIDTH - 1 downto 0);
         iALUOp : in std_logic_vector(ALU_OP_WIDTH - 1 downto 0);
         oResult : out std_logic_vector(DATA_WIDTH - 1 downto 0);
         oCout : out std_logic;
@@ -26,7 +26,7 @@ entity ALU is
         oZero : out std_logic);
 end ALU;
 
-architecture mixed of control is
+architecture mixed of ALU is
 
     component add_sub is
         port(
@@ -54,31 +54,33 @@ signal s_set_less_than_result : std_logic_vector(DATA_WIDTH - 1 downto 0);
 signal s_cout, s_cout2 : std_logic;
 
 signal s_left_shift : std_logic;
+signal s_arithmetic : std_logic;
+signal s_shamt : std_logic_vector(DATA_SELECT - 1 downto 0);
 
 signal s_overflow : std_logic;
 
 begin
 
     -- Set parameters for ALU components
-    s_subtract <= iALUOp(0);
-    s_left_shift <= NOT iALUOP(1);
-    s_arithmetic_shift <= iALUOP(0);
+    --s_subtract <= iALUOp(0);
+    --s_left_shift <= NOT iALUOP(1);
+    --s_arithmetic <= iALUOP(0);
     
     add_sub_C: add_sub
     port map(
-        iSubtract => s_subtract,
-        iA	    => iX1,
-        iB	    => iX2,
+        iSubtract => iALUOp(0),
+        iA	    => iA,
+        iB	    => iB,
         oSum	=> s_add_sub_result,
         oCout 	=> s_cout,
         oCout2  => s_cout2);
 
     barrel_shifter_C: barrel_shifter
     port map(
-        iA => iX1,
-        iLeft => s_left_shift,
-        iArithmetic => s_iArithmetic,
-        iShamt => s_iShamt,
+        iA => iA,
+        iLeft => NOT iALUOP(1),
+        iArithmetic => iALUOP(0),
+        iShamt => s_shamt,
         oResult => s_barrel_shifter_result);
 
 
@@ -88,7 +90,7 @@ begin
     oOverflow <= s_overflow;
     
     -- Set less than result using Overflow detect and result from (a-b)
-    s_set_less_than_result <= "000000000000000000000000000000" & (s_add_sub_result(DATA_WIDTH - 1) XOR s_overflow)
+    s_set_less_than_result <= "0000000000000000000000000000000" & (s_add_sub_result(DATA_WIDTH - 1) XOR s_overflow);
 
     -- Set carry out bit
     oCout <= s_cout;
@@ -97,16 +99,14 @@ begin
     -- Select ALU result
     with iALUOP select
         oResult <=
-            s_add_sub_result when "001-"
+            s_add_sub_result when "001-",
             iA AND iB when "0100",
             iA OR iB when "0101",
             iA XOR iB when "0111",
             iA NOR iB when "0110",
             s_barrel_shifter_result when "10--",
-            s_set_less_than_result when "11--";
-            -- sll when 1000
-            -- srl when 1010
-            -- sra when 1011
+            s_set_less_than_result when "11--",
+            s_barrel_shifter_result when others;
     
     -- Set result bit
     with oResult select
