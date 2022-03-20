425cf3c706aa6b10d17b0bbbbc684d8815009f01
jamesminardi <jamesminardip@gmail.com>
2022-03-19 23:58:43 -0500

    Adjust muxes

--- src/fetch.vhd
+++ src/fetch.vhd
@@ -22,10 +22,10 @@ architecture behavior of fetch is
 	component mux2t1_N is --mux's
   	  	generic(N : integer);
   		port(
-		        i_S          : in std_logic;
-   	            i_D0         : in std_logic_vector(N-1 downto 0);
-             	i_D1         : in std_logic_vector(N-1 downto 0);
-	     	    o_O          : out std_logic_vector(N-1 downto 0));
+		    i_S 	: in std_logic;
+   	        i_D0	: in std_logic_vector(N-1 downto 0);
+            i_D1	: in std_logic_vector(N-1 downto 0);
+	     	o_O 	: out std_logic_vector(N-1 downto 0));
 	end component;
 
 	
@@ -59,6 +59,9 @@ architecture behavior of fetch is
 signal s_PCPlus4 		: std_logic_vector(DATA_SELECT - 1 downto 0);
 signal s_BranchImmShift : std_logic_vector(DATA_SELECT - 1 downto 0);
 signal s_JumpImmShift 	: std_logic_vector(DATA_SELECT - 1 downto 0); -- Only 28 lsb are used
+signal s_BranchTarget	: std_logic_vector(DATA_SELECT - 1 downto 0); -- Final branch address
+signal s_JumpTarget		: std_logic_vector(DATA_SELECT - 1 downto 0); -- Final jump address
+signal s_NoJump			: std_logic_vector(DATA_SELECT - 1 dowtno 0); -- Either Branch address or next instruction address
 	        
 
 begin 
@@ -90,14 +93,16 @@ begin
 				iShamt       => "00010",
 				oResult      => s_BranchImmShift);
 
+	s_JumpTarget <= s_PCPlus4(Data_Width - 1 downto 28) & s_JumpImmShift(27 downto 0);
+
 	ADD_ALU: full_adder_N
 	 --(pc+4) + shift2 to branch mux
 		port map (
-				iA			 => oSum, --PC+4
-				iB			 => iB, -- shift2 sign ext
-				iCin		 => s_inner_carry, --carry in
-				oSum		 => oSum, -- PC+4 + shift2 sign ext
-				oCout 		 => oCout); -- carry out
+				iA			 => s_PCPlus4,
+				iB			 => s_BranchImmShift, -- shift2 sign ext
+				iCin		 => '0',
+				oSum		 => s_BranchTarget, -- PC+4 + shift2 sign ext
+				oCout 		 => open);
 
 	AND_1: andg2
 	 -- and for the branch mux
@@ -108,17 +113,17 @@ begin
 
 	B_MUX: mux2t1_N
 		port map (
-				i_S          => s_andres, -- select = alu branch AND ALU zero
-			    i_D0         => i_D0(i), -- 0 = result of add4
-	    		i_D1         => i_D1(i), -- 1 = result of add alu (add4 + shifted sign ext)
-				o_O          => o_O);	-- goes to jump mux D1
+				i_S          => i_Branch, -- select = alu branch AND ALU zero
+			    i_D0         => s_PCPlus4, -- 0 = result of add4
+	    		i_D1         => s_BranchTarget, -- 1 = result of add alu (add4 + shifted sign ext)
+				o_O          => s_NoJump);	-- goes to jump mux D1
 
 	J_MUX: mux2t1_N
 		port map (
-				i_S          => i_S, --jump select = alu control jump
-   	            i_D0         => i_D0(i), --0 = result of the branch mux
-             	i_D1         => i_D1(i), --1 = jump addr (31-0)
-	     	    o_O          => s_NextInstAddr); --output goes to PC (next inst addr in processor)
+				i_S          => i_Jump, --jump select = alu control jump
+   	            i_D0         => s_NoJump, --0 = result of the branch mux
+             	i_D1         => s_JumpTarget, --1 = jump addr (31-0)
+	     	    o_O          => o_Addr); --output goes to PC (next inst addr in processor)
 
 
 	
