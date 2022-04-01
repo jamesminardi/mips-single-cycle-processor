dc247d6d9914b87e5076569db195e39e7583351a
jamesminardi <jamesminardip@gmail.com>
2022-04-01 13:27:12 -0500

    Update barrel shifter tb

--- test/ALU/tb_barrel_shifter.vhd
+++ test/ALU/tb_barrel_shifter.vhd
@@ -44,55 +44,134 @@ begin
     P_TB: process
     begin
 
--- Test shift right logical
-        s_iA <= x"F1E2D3C4";
+
+------------------------ SHIFT RIGHT ARITHMETIC ------------------------
+
+-- Test shift right arithmetic (0-1)
+        s_iA <= x"80000000";
         s_iLeft <= '0';
-        s_iArithmetic <= '0';
-        s_iShamt <= "00100"; -- Shift 4
+        s_iArithmetic <= '1';
+        s_iShamt <= "00000"; -- Shift 0
         -- Expected:
-        --  oResult = x"0F1E2D3C"
+        --  oResult = x"80000000"
         wait for 100 ns;
 
--- Test shift right logical 2
-        s_iA <= x"F1E2D3C4"; -- 1111_0001_1110_0010_1101_0011_1100_0100
+-- Test shift right arithmetic (1-2)
+        s_iA <= x"80000000";
         s_iLeft <= '0';
-        s_iArithmetic <= '0';
-        s_iShamt <= "00011"; -- Shift 3
+        s_iArithmetic <= '1';
+        s_iShamt <= "00001"; -- Shift 1
         -- Expected:
-        --  oResult = x"1E3C5A78", 0001_1110_0011_1100_0101_1010_0111_1000
+        --  oResult = x"C0000000"
         wait for 100 ns;
 
--- Test shift right arithmetic
-        s_iA <= x"F1E2D3C4";
+-- Test shift right arithmetic (2-3)
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
+        s_iArithmetic <= '1';
+        s_iShamt <= "00010"; -- Shift 2
+        -- Expected:
+        --  oResult = x"E0000000"
+        wait for 100 ns;
+
+-- Test shift right arithmetic (3-4)
+        s_iA <= x"80000000";
         s_iLeft <= '0';
         s_iArithmetic <= '1';
         s_iShamt <= "00100"; -- Shift 4
         -- Expected:
-        --  oResult = x"FF1E2D3C"
+        --  oResult = x"F8000000"
         wait for 100 ns;
 
--- Test shift left logical
-        s_iA <= x"F1E2D3C4"; -- x"23CB478F" backwards
-        s_iLeft <= '1';
+-- Test shift right arithmetic (4-5)
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
+        s_iArithmetic <= '1';
+        s_iShamt <= "01000"; -- Shift 8
+        -- Expected:
+        --  oResult = x"FF800000"
+        wait for 100 ns;
+
+-- Test shift right arithmetic (5-6)
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
+        s_iArithmetic <= '1';
+        s_iShamt <= "10000"; -- Shift 16
+        -- Expected:
+        --  oResult = x"FFFF8000"
+        wait for 100 ns;
+
+
+------------------------ SHIFT RIGHT LOGICAL ------------------------
+
+-- Test shift right logical (0 bit) 6-7
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
+        s_iArithmetic <= '0';
+        s_iShamt <= "00000"; -- Shift 0
+        -- Expected:
+        --  oResult = x"80000000"
+        wait for 100 ns;
+
+-- Test shift right logical (1 bit) 7-8
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
+        s_iArithmetic <= '0';
+        s_iShamt <= "00001"; -- Shift 1
+        -- Expected:
+        --  oResult = x"40000000"
+        wait for 100 ns;
+
+-- Test shift right logical (2 bit) 8-9
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
+        s_iArithmetic <= '0';
+        s_iShamt <= "00010"; -- Shift 2
+        -- Expected:
+        --  oResult = x"20000000"
+        wait for 100 ns;
+
+-- Test shift right logical (4 bit) 9-10
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
         s_iArithmetic <= '0';
         s_iShamt <= "00100"; -- Shift 4
         -- Expected:
-        --  oResult = x"3CB478F0"
+        --  oResult = x"080000000"
+        wait for 100 ns;
+
+-- Test shift right logical (8 bit) 10-11
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
+        s_iArithmetic <= '0';
+        s_iShamt <= "01000"; -- Shift 8
+        -- Expected:
+        --  oResult = x"00800000"
+        wait for 100 ns;
+
+-- Test shift right logical (16 bit) 11-12
+        s_iA <= x"80000000";
+        s_iLeft <= '0';
+        s_iArithmetic <= '0';
+        s_iShamt <= "10000"; -- Shift 16
+        -- Expected:
+        --  oResult = x"00008000"
         wait for 100 ns;
 
 
+------------------------ SHIFT LEFT LOGICAL ------------------------
 
--- Test shift left logical (0 bit)
+-- Test shift left logical (0 bit) 12-13
         s_iA <= x"00000001";
         s_iLeft <= '1';
         s_iArithmetic <= '0';
-        s_iShamt <= "00000"; -- Shift 1
+        s_iShamt <= "00000"; -- Shift 0
         -- Expected:
         --  oResult = x"00000001"
         wait for 100 ns;
 
 
--- Test shift left logical (1 bit)
+-- Test shift left logical (1 bit) 13-14
         s_iA <= x"00000001";
         s_iLeft <= '1';
         s_iArithmetic <= '0';
@@ -101,7 +180,7 @@ begin
         --  oResult = x"00000002"
         wait for 100 ns;
 
--- Test shift left logical (2 bit)
+-- Test shift left logical (2 bit) 14-15
         s_iA <= x"00000001";
         s_iLeft <= '1';
         s_iArithmetic <= '0';
@@ -110,7 +189,7 @@ begin
         --  oResult = x"00000004"
         wait for 100 ns;
 
--- Test shift left logical (4 bit)
+-- Test shift left logical (4 bit) 15-16
         s_iA <= x"00000001";
         s_iLeft <= '1';
         s_iArithmetic <= '0';
@@ -119,7 +198,7 @@ begin
         --  oResult = x"00000010"
         wait for 100 ns;
 
--- Test shift left logical (8 bit)
+-- Test shift left logical (8 bit) 16-17
         s_iA <= x"00000001";
         s_iLeft <= '1';
         s_iArithmetic <= '0';
@@ -129,7 +208,7 @@ begin
         wait for 100 ns;
 
 
--- Test shift left logical (16 bit)
+-- Test shift left logical (16 bit) 17-18
         s_iA <= x"00000001";
         s_iLeft <= '1';
         s_iArithmetic <= '0';
